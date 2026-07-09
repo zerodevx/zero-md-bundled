c3989265229facf9559961497622ef93a8ccaac4
Jason Lee <jason@zerodevx.com>
2026-07-09 15:35:10 +0800

    chore: setup linting

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'eslint/config'
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser
+    },
+    rules: {
+      'no-tabs': 'error',
+      'no-unexpected-multiline': 'error'
+    }
+  },
+  { ignores: ['dist/', 'temp/'] }
+])
